8a50c52366b1da5acd758d060ebd0cf13f7ca3ae
Suneel <Suneel.Kanamarlapudi@cdk.com>
2019-08-24 14:28:14 +0530

    Added a webpack.config file and configured the entry point as src index file and output as main

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 /**
  * Created by kanamars on 24/08/19.
  */
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+      filename: "main.js",
+      path:path.resolve(__dirname, 'dist')
+  }
+};
